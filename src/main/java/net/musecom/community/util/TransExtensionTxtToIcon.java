633a9e66d91137b608abdf98a2c7d522aa32c6a6
OurOh <ahn980321@gmail.com>
2024-11-05 17:00:23 +0900

    1105
    1105

---
+++ src/main/java/net/musecom/community/util/TransExtensionTxtToIcon.java
@@ -0,0 +1,11 @@
+package net.musecom.community.util;
+
+public class TransExtensionTxtToIcon {
+	
+	private String extensionIcon;
+	public String setExtensionIcon(String extensionIcon) {
+		
+		return this.extensionIcon;
+	}
+
+}
